4925b3b27c0c8866e81b39b8cd7f18edaa3b361c
Kapil Dadhich <kapildadhich075@gmail.com>
2024-05-30 23:41:11 +0530

    first commit

--- app/components/Amenity.tsx
+++ app/components/Amenity.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BookDashed, GlassWater } from "lucide-react";
+import { BookDashed } from "lucide-react";
 
 export default function Amenity() {
   const amenities = [
@@ -16,15 +16,15 @@ export default function Amenity() {
   return (
     <>
       <div>
-        <h1 className="text-xl  text-yellow-800 font-light mt-5 md:text-2xl lg:text-3xl">
+        <h1 className="text-xl  text-black font-light mt-5 md:text-2xl lg:text-3xl">
           30+ Amenities
         </h1>
 
         <div className="grid grid-cols-2 gap-10 mt-5 md:grid-cols-3 lg:grid-cols-2">
           {amenities.map((amenity, index) => (
             <div className="flex flex-row gap-2 items-center" key={index}>
-              <BookDashed className="text-yellow-700" />
-              <p className="text-sm  text-yellow-600">{amenity}</p>
+              <BookDashed className="text-yellow-500" />
+              <p className="text-sm font-extralight  text-black">{amenity}</p>
             </div>
           ))}
         </div>
--- app/components/ImageCarousel.tsx
+++ app/components/ImageCarousel.tsx
@@ -5,10 +5,6 @@ import { Carousel } from "react-responsive-carousel";
 export default function ImageCarousel() {
   const [showThumbs, setShowThumbs] = useState(window.innerWidth > 900);
 
-  if (typeof window === "undefined") {
-    return null;
-  }
-
   useEffect(() => {
     const handleResize = () => {
       setShowThumbs(window.innerWidth > 768);
@@ -28,24 +24,38 @@ export default function ImageCarousel() {
         thumbWidth={200}
         dynamicHeight={true}
         showThumbs={showThumbs}
+        autoPlay={true}
+        infiniteLoop={true}
       >
         <div className="w-full h-full">
           <img
             src="https://impacthomes.co.in/wp-content/uploads/2023/04/luxury-home.jpg"
             alt="Luxury Home"
-            className="w-full h-full object-cover"
+            className="w-full  object-cover"
           />
         </div>
         <div>
           <img
-            src="https://d27p8o2qkwv41j.cloudfront.net/wp-content/uploads/2016/06/dreamstime_m_4794570-e1467280605773.jpg"
-            className="w-full h-full object-cover"
+            src="https://impacthomes.co.in/wp-content/uploads/2023/04/luxury-home.jpg"
+            className="w-full  object-cover"
           />
         </div>
         <div>
           <img
-            src="https://images.squarespace-cdn.com/content/v1/614b8a3ca23b0a1609d011b0/1633600657482-RD46JYBH5UB94DFT8GFI/lofthouseresidences_hero_innovation2.jpg"
-            className="w-full h-full object-cover"
+            src="https://impacthomes.co.in/wp-content/uploads/2023/04/luxury-home.jpg"
+            className="w-full object-cover"
+          />
+        </div>
+        <div>
+          <img
+            src="https://impacthomes.co.in/wp-content/uploads/2023/04/luxury-home.jpg"
+            className="w-full object-cover"
+          />
+        </div>
+        <div>
+          <img
+            src="https://impacthomes.co.in/wp-content/uploads/2023/04/luxury-home.jpg"
+            className="w-full object-cover"
           />
         </div>
       </Carousel>
